2ff5ae212fd5ffda8e8e11451cd70606cef8d3f5
Lien Le <95431868+lltlien@users.noreply.github.com>
2022-01-04 23:03:19 +0700

    thêm màu

--- Tetrix_code/Tetrix_code/setting.h
+++ Tetrix_code/Tetrix_code/setting.h
@@ -70,7 +70,18 @@ void configure()
 	system("MODE 22, 22");
 
 	HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-
+     GetConsoleScreenBufferInfoEx(hConsoleOutput, &csbiex);
+        csbiex.ColorTable[0] = RGB(255, 0, 24);
+        csbiex.ColorTable[1] = RGB(255, 165, 44);
+        csbiex.ColorTable[2] = RGB(255, 255, 65);
+        csbiex.ColorTable[3] = RGB(0, 128, 24);
+        csbiex.ColorTable[4] = RGB(0, 0, 249);
+        csbiex.ColorTable[5] = RGB(134, 0, 125);
+        csbiex.ColorTable[6] = RGB(248, 139, 194);
+        csbiex.ColorTable[7] = RGB(242, 242, 242);
+        csbiex.ColorTable[8] = RGB(0, 188, 212);
+        csbiex.ColorTable[9] = RGB(20, 20, 20);
+	SetConsoleScreenBufferInfoEx(hConsoleOutput, &csbiex);
 	CONSOLE_SCREEN_BUFFER_INFOEX csbiex;
 	csbiex.cbSize = sizeof(CONSOLE_SCREEN_BUFFER_INFOEX);
 }
