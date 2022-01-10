ed13e99f3058b1fb12f1895f90d89c69bd23b6f5
UIT.21520800 <94096493+21520800@users.noreply.github.com>
2022-01-10 08:47:56 +0700

    Update setting.h

--- Tetrix_code/Tetrix_code/setting.h
+++ Tetrix_code/Tetrix_code/setting.h
@@ -57,19 +57,18 @@ const vector<vector<wstring>> tetromino = {
 		L"........XXXX......XXXX.........."
 	}
 };
-const int nScreenWidth = 39;
+const int nScreenWidth = 52;
 const int nScreenHeight = 21;
 
-const int nBoardWidth = 39;
+const int nBoardWidth = 30;
 const int nBoardHeight = 21;
 
 const wstring detail = L" █▓░╚╝║═";
 void configure()
 {
 	// Set kích thước cửa sổ console
-	system("MODE 39, 22");
+	system("MODE 52, 22");
 	system("color 89");
-
 	HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	CONSOLE_SCREEN_BUFFER_INFOEX csbiex;
